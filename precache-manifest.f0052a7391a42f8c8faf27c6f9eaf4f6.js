306f71e607155eb74f0697526b915b99dac87484
khedher-asma <khedherasma90@gmail.com>
2019-06-27 16:41:04 +0200

    Updates

---
+++ precache-manifest.f0052a7391a42f8c8faf27c6f9eaf4f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0805a5431a8d65164fb2a5c53859606",
+    "url": "/HOC-movie-app/index.html"
+  },
+  {
+    "revision": "cd8a9152ab7ad2ba831c",
+    "url": "/HOC-movie-app/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "01474bc32a97f58f5a48",
+    "url": "/HOC-movie-app/static/css/main.f82eb8b8.chunk.css"
+  },
+  {
+    "revision": "cd8a9152ab7ad2ba831c",
+    "url": "/HOC-movie-app/static/js/2.9da3dce5.chunk.js"
+  },
+  {
+    "revision": "01474bc32a97f58f5a48",
+    "url": "/HOC-movie-app/static/js/main.a785fd99.chunk.js"
+  },
+  {
+    "revision": "231881330f6bd44a20f6",
+    "url": "/HOC-movie-app/static/js/runtime~main.cac58955.js"
+  }
+]);
